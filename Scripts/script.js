dd1f9b36aa9f8d023fed1a2778fd9419dca023f8
Andrew Pritchett <andrenator95@gmail.com>
2020-11-20 22:19:39 -0700

    a lot

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,15 +1,23 @@
 
 var $aboutBtn = $("#aboutBtn");
 var $contactBtn = $("#contactBtn");
+var $classBtn = $("#classBtn");
 
 $aboutBtn.click(function() {
     $('html, body').animate({
         scrollTop: ($('#content').offset().top)
     },500);
 });
+
 $contactBtn.click(function() {
     $('html, body').animate({
         scrollTop: ($('#contact').offset().top)
+    },500);
+});
+
+$classBtn.click(function() {
+    $('html, body').animate({
+        scrollTop: ($('#classTable').offset().top)
     },500);
 });
 
