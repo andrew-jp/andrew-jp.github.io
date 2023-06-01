5eb0caf3608fcb39dbf1d7260d580f8710f29a04
andrew-jp <74275670+andrew-jp@users.noreply.github.com>
2023-06-01 17:45:21 -0600

    I'm an idiot, figured out the theme. Working on sizing of the sample section. Want to size the code column/parent to the svg and desc

--- src/components/Samples.js
+++ src/components/Samples.js
@@ -2,7 +2,7 @@ import React, {useEffect, useRef} from 'react';
 import { marked } from "marked";
 
 import hljs from "highlight.js";
-import "highlight.js/styles/github.css";
+import "highlight.js/styles/base16/atelier-dune.css";
 import ast from "./img/ast.svg"
 
 const Samples = () => {
@@ -42,280 +42,355 @@ const Samples = () => {
   }, []);
 
   const markdown = `
-  \`\`\`
-  /**
-   * This is a test program for my Compiler.
-   * The SVG below is the Abstract Syntax Tree
-   * created by my project.
-  */
-  void kxi2023 main() {
+  \`\`\`C
 
-    // test un-initialized variables
-    int u1;
-    char u2;
-    bool u3;
+  class a {
+    public char xyz;
+    public char x = 'q';
+    public b g() {}
+    private char p = 'p';
+    private char[] arrTempTest;
+    private duplicates duplicates
+      = new duplicates();
+    public char y;
 
-    cout << u1;
-    cout << '\\n';
-    cout << u2;
-    cout << '\\n';
-    if (!u3) cout << '0';
-    cout << '\\n';
-
-    int var = 01;  // this works as 1
-    while (var <= 20) {
-      cout << var;
-      cout << " -> ";
-      if (var / 2 * 2 == var) {
-          cout << "even";
-      } else {
-          cout << "odd";
-      }
-      if (var / 3 * 3 == var) {
-          cout << "/div3";
-      }
-      cout << '\\n';
-      var += 1;
+    public bool paramScopeTest(char xyz,
+                              char p,
+                              char y) {
+        this.xyz = xyz;
+        this.p = p;
+        this.y = y;
     }
 
-    switch(1 + 2 + 3 * 4) {
-      case 0:
-          break;
-      case 15:
-          cout << "long expression switch cond\n";
-          break;
-      default:
-          cout << "switch\\n";
-    }
+    public bool testbool(int testInt,
+                        char testChar) {}
 
-
-    if (1==1) cout << "1==1\\n";
-    if (-1==-1) cout << "-1==-1\\n";
-    if (1==2) cout << "1==2 fail\\n";
-    if (2==1) cout << "2==1 fail\\n";
-
-    if (1!=2) cout << "1!=2\\n";
-    if (2!=1) cout << "2!=1\\n";
-    if (1!=1) cout << "1!=1 fail\\n";
-
-    if (1 < 2) cout << "1<2\\n";
-    if (1 < 1) cout << "1<1 fail\\n";
-    if (2 < 1) cout << "2<1 fail\\n";
-
-    if (2 > 1) cout << "2>1\\n";
-    if (1 > 1) cout << "1>1 fail\\n";
-    if (1 > 2) cout << "1>2 fail\\n";
-
-    if (1 <= 2) cout << "1<=2\\n";
-    if (1 <= 1) cout << "1<=1\\n";
-    if (2 <= 1) cout << "2<=1 fail\\n";
-
-    if (2 >= 1) cout << "2>=1\\n";
-    if (1 >= 1) cout << "1>=1\\n";
-    if (1 >= 2) cout << "1>=2 fail\\n";
-
-    if (-1 == -(1))
-        cout << "-1==-(1)\\n";
-
-    if (1 * 2 / 2 + 4 - 5 * 8 / 8 == 0)
-        cout << "good math\\n";
-
-    if (     'a' == 'a'
-        &&   'b' == 'b'
-        && !('c' == 'd')
-        &&   'c' != 'd'
-        &&   'd' <  'e'
-        &&   'a' >  'A' )
-        cout << "char comparison pass\\n";
-
-    int un_test = 2;
-    un_test = +++++++un_test;
-    cout << un_test;
-    cout << '\\n';
-
-
-    int x = 1;
-    while (x <= 16) {
-        cout << "whl: ";
-        cout << x;
-        cout << '\\n';
-
-        int i = x;
-        int j = 0;
-        while(j < 3) {
-            cout << "nest whl: ";
-            cout << i * 3;
-            cout << '\\n';
-            i *= 3;
-            j += 1;
+    private void q(int j, bool q) {
+        int r = 4 + 3;
+        bool iswut = true || false;
+        char k = 'k';
+        if (k < 'r') {
+            //q(r * 3, p < k);
+            while (testbool(r, xyz)) {
+                
+            }
         }
-        x *= 2;
+        return;
     }
 
-    x = j = i;
-    cout << "should match above value: ";
-    cout << x; cout << '\\n';
-
-    // ASSIGNMENT
-    cout << "assneq: ";
-
-    int test = 0-1;
-    
-    test += 1;
-    if (test == 0) {
-        cout << '+';
-    } else {
-        cout << '-';
-        cout << test;
-    }
-    cout << '|';
-
-    test -= 1;
-    if (test == -1) {
-        cout << '+';
-    } else {
-        cout << '-';
-    }
-    cout << '|';
-
-    test *= 2;
-    if (test == -2) {
-        cout << '+';
-    } else {
-        cout << '-';
-    }
-    cout << '|';
-
-    test /= 2;
-    if (test == -1) {
-        cout << '+';
-    } else {
-        cout << '-';
-    }
-    cout << '|';
-    cout << '\\n';
-
-    // BOOL EXPRESSIONS
-    cout << "bool: ";
-
-    bool t = true;
-    bool f = false;
-    int a = 0;
-    int b = 1;
-
-    if (t) {
-        cout << '+';
-    } else {
-        cout << '-';
-    }
-    cout << '|';
-    if (t != f) {
-        cout << '+';
-    } else {
-        cout << '-';
-    }
-    cout << '|';
-    if (a < b) {
-        cout << '+';
-    } else {
-        cout << '-';
-    }
-    cout << '|';
-    if (b > a) {
-        cout << '+';
-    } else {
-        cout << '-';
-    }
-    cout << '|';
-    if (a <= 0) {
-        cout << '+';
-    } else {
-        cout << '-';
-    }
-    cout << '|';
-    if (b >= 1) {
-        cout << '+';
-    } else {
-        cout << '-';
-    }
-    cout << '|';
-    cout << '\\n';
-    
-
-    // SWITCH CASE
-    cout << "switch: ";
-
-    a = 0;
-    switch (a) {
-        case 1:
-            cout << '-';
-            break;
-        case 0:
-            cout << '.';
-        case 3:
-            cout << ',';
-        default:
-            cout << '+';
-    }
-    cout << '|';
-    cout << '\\n';
-
-    int v1 = 1;
-    int v2 = 2;
-    int v3 = 3;
-
-    v1 = v2 = v3;
-
-    if (v1 == 3 && v2 == 3 && v3 == 3)
-      cout << "v1 = v2 = v3 pass\\n";
-    else
-      cout << "v1 = v2 = v3 fail\\n";
-
-    v1 += v1 = 2;
-    if (v1 == 4)
-      cout << "v1 += v1 = 2 pass\\n";
-    else
-      cout << "v1 += v1 = 2 fail\\n";
-
-    v1 = v2 = v3 = 1 + 2 + 3;
-    if (v1 == 6 && v2 == 6 && v3 == 6)
-      cout <<
-      "v1 = v2 = v3 = 1 + 2 + 3 pass\\n";
-    else
-      cout <<
-      "v1 = v2 = v3 = 1 + 2 + 3 fail\\n";
-
-    int temp = v1 + v3;
-    v1 = v2 = v3 = v1 = v1 + v3;
-    if (v1 == temp)
-      cout << "long \"=\" chain pass\\n";
+    private void emptyFunc() {}
 }
+
+class empty {}
+
+class duplicates {
+    duplicates() {}
+}
+
+class b {
+    public char y = 'a';
+    public int[] numarr = new int[4];
+    public int[] newarr = new int[5];
+    private char v = 'q';
+    public char j;
+    public int twenty = 20;
+
+    public int f(int number, char character) {     
+        this.v = 'v';
+        return numarr[6];
+    } 
+    
+    public int testMore(int number,
+                        char character,
+                        a paramMemberTest,
+                        b paramTest2,
+                        b[] paramArrTest) {     
+        this.v = 'v'; 
+        paramMemberTest.xyz = 'x';
+        paramTest2.y = 'x';
+        paramArrTest[3].j = 'j';
+    }
+
+    public a h(){}
+}
+
+class cat {
+    cat(int kittens) {}
+    public int lmnop = 3;
+}
+
+class Owl { 
+    private int i;
+    public int f() {}
+    public char alpha = 'a';
+    private void g(int i, Owl y) {
+        int a;
+        int[] ii; 
+        Owl[] Owl;
+        Owl[] hoot;
+        Owl thing;
+        Owl x = y;
+        Owl = hoot;
+        i = this.i;
+        return;
+        ii = ii;
+    }
+}
+
+class Mouse {
+    private int weight;
+    private int age = -1;
+    public int eyeCount = 2;
+    public char[] pedigree = new char[4];
+    public char firstInitial = 'x';
+
+    private Cat testCat1 = new Cat(26);
+    public Cat testCat2 = new Cat(26);
+
+    private void Initialize(char[] initials)
+    {
+        eyeCount = 4;
+        this.pedigree = initials;
+        firstInitial = initials[0];
+    }
+
+    Mouse(int age, char[] initials, int weight)
+    {
+        this.age = age;
+        this.weight = weight;
+        Initialize(initials);
+    }
+
+
+
+    public char[] PublicFunc()
+    {
+        int i = 3 + 4 * 2 + 83 /3;
+        int j = (i + 2)/(i * -2) + (-23)-+2+-12;
+        char[] result = new char[4];
+        firstInitial = 'r';
+        while (i >= 0)
+        {
+            result[3 - i] = this.pedigree[i];
+            i = i +-1;
+        }
+    }
+    
+    public NoCtor ClassyFunc(Mouse m,
+                            NoCtor[] things) {
+        NoCtor one = things[0];
+    }
+
+    public Mouse[] BigMethod(bool[] array,
+                            int length,
+                            char[] name,
+                            int nameLength,
+                            char foo,
+                            char[] bar,
+                            Mouse m,
+                            NoCtor n,
+                            Mouse[] mice,
+                            NoCtor[] nox,
+                            bool asdf) {
+        array[0] = true;
+        name[length] = 'c';
+        if (nameLength < length) {
+            array[1] = true;
+        }
+        else {
+            array[1] = false;
+        }
+        bar[0] = foo;
+        if (asdf) {
+            nox[0] = n;
+        }
+        else {
+            nox[1] = n;
+        }
+        m.eyeCount = nameLength / 2 + length;
+        cout << mice[10].eyeCount;
+    }
+
+    public Mouse FeedMouse(int count,
+                          bool healthy) { }
+
+    public int IntTime(bool b, char c) { }
+
+    public int GetWeight(bool b, char c) {
+        return weight;
+    }
+}
+
+class NoCtor { 
+    NoCtor(){}
+}
+
+class Cat {
+    public int MiceEaten = 0;
+    private int Weight = 0;
+    public Owl testOwl;
+
+    Cat(int weight) {
+        this.Weight = weight;
+    }
+
+    public void FeedMouse(Mouse m) {
+        Weight = Weight + m.GetWeight(true, 'A');
+    }
+}
+
+void kxi2023 main() {
+    int x = 12;
+    int y = 20;
+    int g = -4;
+    int f = 0003;
+    int k = +00000533;
+    char l = 'l';
+    int r = 23;
+    int s = 2;
+    bool bubs = true;
+    char[] c = new char[2+4];
+    bool relationalBoolTest;
+    a atest = null;
+    a a = atest;
+
+    cat cat = new cat(r);
+    cat[] kitty = new cat[r];
+    b testingArrStuff;
+
+    cat kitten = null;
+    int[] numNull = null;
+
+    int i = 3 + 4 * 2 + 83 /3;
+    int j = (i + 2)/(i * -2)
+            + (-23)-+2+-12--1++2;
+    int[] ints = new int[10];
+    bool b;
+    bool[] bools = new bool[2];
+    char alpha = 'A';
+    char[] chs = new char[j];
+    char[] foo;
+    Mouse[] mice = new Mouse[ints[4]];
+    Mouse mouse1;
+    Mouse mouse2 = new Mouse(
+                      -24 * (i + 2) /
+                      (i * -2) + (-23)
+                      -+2+-12,
+                      null,
+                      (i + 2)/(i * -2)
+                      + (-23)-+2+-12);
+    Cat cat1 = new Cat(24);
+    NoCtor noct = new NoCtor();
+    NoCtor[] noctArr;
+    
+    chs[0] = 'H';
+    chs[1] = '\n';
+    mouse1 = new Mouse(100, chs, 1);
+    mice[i + j]
+        .FeedMouse(i, false)
+            .BigMethod(bools,
+                      ints[2],
+                      chs,
+                      j + 1,
+                      foo[j - i],
+                      foo,
+                      mouse2,
+                      null,
+                      mice,
+                      null,
+                      i < j);
+    mice[i + j]
+        .FeedMouse(i, false)
+            .BigMethod(bools,
+                      ints[2],
+                      chs,
+                      j + 1,
+                      foo[j - i],
+                      foo,
+                      mouse2,
+                      noct,
+                      mice,
+                      noctArr,
+                      i < j);
+
+    cin >> mouse1.pedigree[i + j];
+    cin >> mouse1.pedigree[i-+2+-12];
+
+    alpha = mouse1.testCat2.testOwl.alpha;
+    
+    cout << mouse1.pedigree[1];
+    cin >> mouse1.eyeCount;
+    cin >> mouse1
+              .pedigree[
+                (i + 2)/(i * -2)
+                + (-23)-+2+-12--1++2
+              ];
+    mouse1.PublicFunc();
+    b = j + 12 * (i - j) < j;
+    ints[0] = ints[1] + ints[2];
+    b = ints[2] < ints[i];
+    mouse1.eyeCount = mouse2.eyeCount;
+    mouse1.pedigree[1] = mouse2.pedigree[1];
+    i = mouse1
+            .IntTime(b, alpha)
+            + j * ints[99 * 2];
+    mouse1
+        .FeedMouse(1, true)
+            .FeedMouse(2, true)
+                .FeedMouse(3, false)
+                    .eyeCount = ints[10];
+    ints = null;
+    mouse2 = null;
+    cat1.FeedMouse(mouse1);
+    cat1.FeedMouse(mice[i - j]);
+
+    c[s + 3] = c[s - 2];
+
+    testingArrStuff.newarr[4]
+        = x - 20 + testingArrStuff
+            .newarr[2 + 3];
+    testingArrStuff.twenty
+        = testingArrStuff.twenty + -1 + 1;
+    kitty[2].lmnop = kitty[2].lmnop + 4;
+
+    x = y;
+
+    x = y * g + f / k;
+
+    cout << c[1+3-2*1];
+    relationalBoolTest = x + 12 * (y - g) < r;
+
+    x;
+}
+
   \`\`\`
   `;
 
   return (
     <div className="sampleSection">
-      <div className="leftSample">
-        <p ref={targetRef} className="projDesc">
-          &emsp;&emsp;For my senior capstone, I designed and built
-          a compiler that handles a Java-like language. This is a small
-          program I was testing execution on. To the right is an AST
-          representing the source code as a data structure - read by a visitor
-          (one of many) and output in <i>DOT</i> (GraphViz) syntax. The colors
-          are a byproduct of my semantic analysis and type checking - when a 
-          node has been validated for type correctness it's marked green. 
-          Generally I don't mark the higher level statements (cout, while)
-          because they're validated elsewhere.
-        </p>
+      <div className='leftSample'>
         <pre className="codeSection">
           <code dangerouslySetInnerHTML={{__html: marked(markdown)}}></code>
         </pre>
       </div>
-      <img
-        className="graph"
-        src={ast}
-        alt="KXI AST"
-      />
+      <div className='rightSample'>
+        <p ref={targetRef} className="projDesc">
+            &emsp;&emsp;For my senior capstone, I designed and built
+            a compiler that handles a Java-like language. The code is KXI, a UVU
+            developed programming language. To the right is an AST
+            representing the source code as a data structure - read by a visitor
+            (one of many) and output in <i>DOT</i> (GraphViz) syntax. The colors
+            are a byproduct of my semantic analysis and type checking - when a 
+            node has been validated for type correctness it's marked green. 
+            Generally I don't mark the higher level statements (cout, while)
+            because they're validated elsewhere.
+        </p>
+        <img
+          className="graph"
+          src={ast}
+          alt="KXI AST"
+        />
+      </div>
     </div>
   );
 }
